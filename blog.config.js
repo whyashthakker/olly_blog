922dde01c820070e25d68769816f80c5825b1f9a
whyashthakker <nynansnh@gmail.com>
2024-01-14 15:58:48 +0530

    blog about

--- blog.config.js
+++ blog.config.js
@@ -14,7 +14,7 @@ const BLOG = {
   since: 2024, // If leave this empty, current year will be used.
   postsPerPage: 7,
   sortByDate: false,
-  showAbout: true,
+  showAbout: false,
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
